5743f319e62683ca972aea35a0a3dd9bc1c79706
nikpolik <n.a.polikandriotis@gmail.com>
2021-08-31 12:56:49 +0300

    Use attr method to assign constantly changing Level styles

--- src/Components/Feed/Level.tsx
+++ src/Components/Feed/Level.tsx
@@ -1,23 +1,29 @@
-import styled from 'styled-components';
-import { Color } from '../../theme/colors';
+import styled from "styled-components";
+import { Theme } from "../../theme";
+import { Color } from "../../theme/colors";
 
 interface LevelProps {
   maxLevel: number;
   currentLevel: number;
   color: Color;
   direction: "left" | "right";
+  theme: Theme;
 }
 
 const getLevelWidth = ({ maxLevel, currentLevel }: LevelProps): number => {
   return Math.round((currentLevel / maxLevel) * 100);
 };
 
-const Level = styled.div<LevelProps>`
+const Level = styled.div.attrs((props: LevelProps) => ({
+  style: {
+    backgroundColor: props.theme.colors[props.color],
+    left: props.direction === "left" ? "-8px" : undefined,
+    right: props.direction === "right" ? "-8px" : undefined,
+    width: `${getLevelWidth(props)}%`,
+  },
+}))<LevelProps>`
   position: absolute;
-  width: ${getLevelWidth}%;
   height: 100%;
-  background-color: ${({ theme, color }) => theme.colors[color]};
-  ${({ direction }) => `${direction}: -8px`};
   z-index: -1;
 `;
 
